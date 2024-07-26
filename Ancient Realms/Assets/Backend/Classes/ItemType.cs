049eb4a68b5aa38b985a92ca6a31ac723c96d9c1
Caesar <joseph092702@gmail.com>
2024-07-26 14:09:58 +0800

    Database Update

---
+++ Ancient Realms/Assets/Backend/Classes/ItemType.cs
@@ -0,0 +1,13 @@
+using System;
+
+
+namespace ESDatabase.Classes
+{
+    [Serializable]
+    public class ItemType
+    {
+        public int typeID {get;set;}
+        public string typeName {get;set;}
+    }
+}
+
