75ac40d8fb419b5b9cd254b39a3e8856e0e059f6
Regannn <regann0514@gmail.com>
2024-08-01 20:28:29 +0800

    Sword DragNDrop

--- Ancient Realms/Assets/DragNDrop/Scripts/DragDrop.cs
+++ Ancient Realms/Assets/DragNDrop/Scripts/DragDrop.cs
@@ -5,14 +5,13 @@ using UnityEngine.EventSystems;
 
 public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-
     [SerializeField] private Canvas canvas;
     private RectTransform rectTransform;
     private CanvasGroup canvasGroup;
 
     private void Awake(){
-       rectTransform = GetComponent <RectTransform>();
-       canvasGroup = GetComponent <CanvasGroup>();
+       rectTransform = GetComponent<RectTransform>();
+       canvasGroup = GetComponent<CanvasGroup>();
     }
 
     public void OnBeginDrag(PointerEventData eventData){
@@ -25,19 +24,14 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
         Debug.Log("OnDrag");
         rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
     }
-     public void OnEndDrag(PointerEventData eventData){
+
+    public void OnEndDrag(PointerEventData eventData){
         Debug.Log("OnEndDrag");
         canvasGroup.alpha = 1f;
         canvasGroup.blocksRaycasts = true;
     }
 
-     public void OnPointerDown(PointerEventData eventData){
+    public void OnPointerDown(PointerEventData eventData){
         Debug.Log("OnPointerDown");
     }
-
-    public void OnDrop(PointerEventData eventData) {
-        throw new System.NotImplementedException();
-    }
 }
-
-
