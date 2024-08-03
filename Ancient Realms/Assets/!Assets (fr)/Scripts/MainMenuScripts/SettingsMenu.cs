16642b7df56e9bd421c749dda68929125572ead6
Reisi <archievillar23@gmail.com>
2024-08-03 20:42:54 +0800

    Volume settings

--- Ancient Realms/Assets/!Assets (fr)/Scripts/MainMenuScripts/SettingsMenu.cs
+++ Ancient Realms/Assets/!Assets (fr)/Scripts/MainMenuScripts/SettingsMenu.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using ESDatabase.Entities;
 using UnityEngine;
 using UnityEngine.Audio;
-using UnityEngine.Rendering;
 using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
@@ -13,56 +12,88 @@ public class SettingsMenu : MonoBehaviour
     [SerializeField] SettingsAnimation settingsPanel;
     [SerializeField] GameObject mainMenuPanel;
     [SerializeField] GameObject confirmationPanel;
+
     [Header("Slider")]
     [SerializeField] Slider masterSlider;
     [SerializeField] Slider musicSlider;
+    [SerializeField] Slider soundFXSlider; 
+
     public AudioMixer audioMixer;
     public float masterVolume, oldMasterVolume;
     public float musicVolume, oldMusicVolume;
+    public float soundFXVolume, oldSoundFXVolume; 
 
-    async void Start(){
+    async void Start()
+    {
         oldMusicVolume = musicSlider.value;
         oldMasterVolume = masterSlider.value;
+        oldSoundFXVolume = soundFXSlider.value; 
     }
+
     public void SetMasterVolume(float volume)
     {
         masterVolume = volume;
     }
+
     public void SetMusicVolume(float volume)
     {
         musicVolume = volume;
     }
+
+    public void SetSoundFXVolume(float volume)
+    {
+        soundFXVolume = volume;
+    }
+
     public async void SaveSettings()
     {
         PlayerData playerData = await AccountManager.GetPlayer();
 
-        // Modify the entity
-        if(oldMasterVolume != masterVolume) playerData.gameData.settings.masterVolume = masterVolume;
-        if(oldMusicVolume != musicVolume) playerData.gameData.settings.musicVolume = musicVolume;
-        // Save the modified entity
-        if(oldMasterVolume != masterVolume || oldMusicVolume != musicVolume) await AccountManager.SaveData(playerData);
+        
+        if (oldMasterVolume != masterVolume) playerData.gameData.settings.masterVolume = masterVolume;
+        if (oldMusicVolume != musicVolume) playerData.gameData.settings.musicVolume = musicVolume;
+        if (oldSoundFXVolume != soundFXVolume) playerData.gameData.settings.soundFXVolume = soundFXVolume; 
+
+        
+        if (oldMasterVolume != masterVolume || oldMusicVolume != musicVolume || oldSoundFXVolume != soundFXVolume)
+        {
+            await AccountManager.SaveData(playerData);
+        }
+
         oldMasterVolume = masterVolume;
         oldMusicVolume = musicVolume;
+        oldSoundFXVolume = soundFXVolume; 
+
         settingsPanel.Close();
         mainMenuPanel.SetActive(true);
-        audioMixer.SetFloat("volume", masterVolume);
+        audioMixer.SetFloat("masterVolume", masterVolume); 
+        audioMixer.SetFloat("musicVolume", musicVolume); 
+        audioMixer.SetFloat("soundFXVolume", soundFXVolume); 
     }
+
     public async void CheckChanges()
     {
         bool changed = false;
-        if(oldMasterVolume != masterVolume) changed = true;
-        if(oldMusicVolume != musicVolume) changed = true;
-        
-        if(changed == true){
+        if (oldMasterVolume != masterVolume) changed = true;
+        if (oldMusicVolume != musicVolume) changed = true;
+        if (oldSoundFXVolume != soundFXVolume) changed = true; 
+
+        if (changed == true)
+        {
             confirmationPanel.SetActive(true);
             settingsPanel.Close();
-        }else{
+        }
+        else
+        {
             settingsPanel.Close();
             mainMenuPanel.SetActive(true);
         }
     }
-    public void Retain(){
+
+    public void Retain()
+    {
         musicSlider.value = musicVolume;
         masterSlider.value = masterVolume;
+        soundFXSlider.value = soundFXVolume; 
     }
 }
--- Ancient Realms/Assets/Backend/Classes/SettingsData.cs
+++ Ancient Realms/Assets/Backend/Classes/SettingsData.cs
@@ -1,18 +1,20 @@
 using System;
 using Unisave;
 
-
 namespace ESDatabase.Classes
 {
     [Serializable]
     public class SettingsData
     {
-        [Fillable] public float masterVolume {get;set;}
-        [Fillable] public float musicVolume {get;set;}
-        public SettingsData(){
+        [Fillable] public float masterVolume { get; set; }
+        [Fillable] public float musicVolume { get; set; }
+        [Fillable] public float soundFXVolume { get; set; } 
+
+        public SettingsData()
+        {
             masterVolume = 100;
             musicVolume = 100;
+            soundFXVolume = 100; 
         }
     }
 }
-
