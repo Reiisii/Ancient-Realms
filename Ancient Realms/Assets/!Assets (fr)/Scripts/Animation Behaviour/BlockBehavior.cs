60b66ffe0b7b11ccbdd78b3a8b4a90007db90b5e
Caesar <joseph092702@gmail.com>
2024-08-12 11:00:34 +0800

    Combat Update + Stamina Consumption + Blocking + Attack Fix

--- Ancient Realms/Assets/!Assets (fr)/Scripts/Animation Behaviour/BlockBehavior.cs
+++ Ancient Realms/Assets/!Assets (fr)/Scripts/Animation Behaviour/BlockBehavior.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CombatIdleBehaviour : StateMachineBehaviour
+public class BlockBehavior : StateMachineBehaviour
 {
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         PlayerController.GetInstance().canReceiveInput = true;
-        PlayerController.GetInstance().canWalk = true;
+        PlayerController.GetInstance().canWalk = false;
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
