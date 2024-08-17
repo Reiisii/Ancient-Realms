17edd79f839b4b8b42e53900413ee415126167e2
Caesar <joseph092702@gmail.com>
2024-08-17 15:49:57 +0800

    Movement Bash

--- Ancient Realms/Assets/!Assets (fr)/Scripts/Animation Behaviour/ShieldBashBehavior.cs
+++ Ancient Realms/Assets/!Assets (fr)/Scripts/Animation Behaviour/ShieldBashBehavior.cs
@@ -2,27 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockWalkBehavior : StateMachineBehaviour
+public class ShieldBashBehavior : StateMachineBehaviour
 {
-    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        PlayerController.GetInstance().isBlocking = true;
-    }
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-        if(PlayerController.GetInstance().isAttacking){
-            PlayerController.GetInstance().isBlocking = false;
-            animator.SetBool("isBlocking", false);
-            animator.Play("Combat Shield Bash");
-        }
-    }
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        PlayerController.GetInstance().isAttacking = false;
+       PlayerController.GetInstance().isAttacking = false;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
