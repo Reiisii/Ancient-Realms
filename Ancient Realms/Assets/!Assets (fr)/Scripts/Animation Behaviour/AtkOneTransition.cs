73c0c539c4b4c24503a47721c4c5849ac70fc188
Caesar <joseph092702@gmail.com>
2024-09-04 11:02:39 +0800

    Animation Adjustment, Audio Adjustment, Combat Attack and Equip

--- Ancient Realms/Assets/!Assets (fr)/Scripts/Animation Behaviour/AtkOneTransition.cs
+++ Ancient Realms/Assets/!Assets (fr)/Scripts/Animation Behaviour/AtkOneTransition.cs
@@ -6,20 +6,21 @@ public class AtkOneTransition : StateMachineBehaviour
 {
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-
+        PlayerController.GetInstance().isAttacking = false;
     }
     
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         if(PlayerController.GetInstance().isAttacking){
+            PlayerController.GetInstance().isAttacking = true;
             animator.Play("Roman Normal Attack 2");
         }  
     }
 
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        PlayerController.GetInstance().isAttacking = false;
+        // PlayerController.GetInstance().isAttacking = false;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
--- Ancient Realms/Assets/AtkOneStatic.cs
+++ Ancient Realms/Assets/AtkOneStatic.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnequipBehavior : StateMachineBehaviour
+public class AtkOneStatic : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        if(!PlayerStats.GetInstance().isCombatMode && !animator.GetBool("isCombatMode") && PlayerController.GetInstance().isEquipping){
-            PlayerController.GetInstance().canWalk = false;
-            PlayerController.GetInstance().IsRunning = false;
-            PlayerController.GetInstance().IsMoving = false;
-            animator.Play("Unequip");
-        }
+       PlayerController.GetInstance().isAttacking = false;
     }
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
