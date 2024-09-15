c93bf7985b4aed553f52a8c40729bc2d8348352f
Caesar <joseph092702@gmail.com>
2024-09-15 20:34:30 +0800

    Patch

--- Ancient Realms/Assets/!Assets (fr)/Scripts/Managers/PlayerUIManager.cs
+++ Ancient Realms/Assets/!Assets (fr)/Scripts/Managers/PlayerUIManager.cs
@@ -3,18 +3,33 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using DG.Tweening;
 using ESDatabase.Entities;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerUIManager : MonoBehaviour
 {
-    [Header("Game Objects")]
-    [SerializeField] CanvasGroup backgroundCanvasGroup;
-    [SerializeField] GameObject playerUI;
-    [SerializeField] CanvasGroup playerCanvasGroup;
-    [SerializeField] GameObject loadingScreen;
-    [SerializeField] CanvasGroup loadingCanvasGroup;
-    [SerializeField] CanvasGroup fade;
+    [Header("Background")]
+    [SerializeField] public GameObject backgroundGO;
+    [SerializeField] public CanvasGroup backgroundCanvasGroup;
+    [Header("Player UI")]
+    [SerializeField] public GameObject playerUI;
+    [SerializeField] public CanvasGroup playerCanvasGroup;
+    [Header("Loading Screen")]
+    [SerializeField] public GameObject loadingScreen;
+    [SerializeField] public CanvasGroup loadingCanvasGroup;
+    [Header("Fade")]
+    [SerializeField] public CanvasGroup fade;
+    [SerializeField] public GameObject fadeGO;
+    [Header("Map")]
+    [SerializeField] public CanvasGroup mapCanvasGroup;
+    [SerializeField] public GameObject mapGO;
+    [SerializeField] public GameObject worldMap;
+    [Header("Location Plaque")]
+    [SerializeField] public GameObject locationPlaque;
+    [SerializeField] public TextMeshProUGUI locationText;
+    [Header("Achievement Plaque")]
+    [SerializeField] public GameObject achievementPlaque;
     [Header("Settings")]
     [SerializeField] float fadeDuration;    
     [SerializeField] EaseTypes fadeEaseType;
@@ -48,28 +63,51 @@ public class PlayerUIManager : MonoBehaviour
 
     public async Task OpenPlayerUI(){
         playerUI.SetActive(true);
+        playerCanvasGroup.interactable = false;
         await playerCanvasGroup.DOFade(1, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
+        playerCanvasGroup.interactable = true;
     }
     public async Task ClosePlayerUI(){
+        playerCanvasGroup.interactable = false;
         await playerCanvasGroup.DOFade(0, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
+        playerUI.SetActive(false);
     }
     public async Task OpenLoadingUI(){
+        loadingScreen.SetActive(true);
         await loadingCanvasGroup.DOFade(1, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
     }
     public async Task CloseLoadingUI(){
         await loadingCanvasGroup.DOFade(0, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
+        loadingScreen.SetActive(false);
     }
     public async Task OpenDarkenUI(){
+        fadeGO.SetActive(true);
         await fade.DOFade(1, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
     }
     public async Task CloseDarkenUI(){
         await fade.DOFade(0, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
+        fadeGO.SetActive(false);
     }
     public async Task OpenBackgroundUI(){
+        backgroundGO.SetActive(true);
         await backgroundCanvasGroup.DOFade(1, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
     }
     public async Task CloseBackgroundUI(){
         await backgroundCanvasGroup.DOFade(0, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
+        backgroundGO.SetActive(false);
+    }
+    public async Task OpenMapUI(){
+        mapGO.SetActive(true);
+        mapCanvasGroup.interactable = false;
+        worldMap.SetActive(true);
+        await mapCanvasGroup.DOFade(1, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
+        mapCanvasGroup.interactable = true;
+    }
+    public async Task CloseMapUI(){
+        mapCanvasGroup.interactable = false;
+        await mapCanvasGroup.DOFade(0, fadeDuration).SetEase((Ease)fadeEaseType).SetUpdate(true).AsyncWaitForCompletion();
+        mapGO.SetActive(false);
+        worldMap.SetActive(false);
     }
     private async void OnSceneLoaded(AsyncOperation operation)
     {
